830d9661a1e39f071879c0db886514d49e0eee83
gustavoquinalha <gustavo_quinalha@hotmail.com>
2018-09-19 18:07:28 +0000

    Create template

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cortecloud Storybook - Design system
+> Examples: https://github.com/storybooks/storybook/tree/master/examples/html-kitchen-sink
---
+++ stories/index.stories.js
@@ -0,0 +1,83 @@
+import { document } from 'global';
+import { storiesOf } from '@storybook/html';
+import { action } from '@storybook/addon-actions';
+import { withLinks } from '@storybook/addon-links';
+
+// css
+import '../assets/css/master.css';
+
+// components
+import welcome from '../assets/components/welcome.html';
+
+storiesOf('Components', module)
+  .add('Welcome', () => welcome);
+
+storiesOf('Atoms', module)
+
+  .add(
+    'Branding',
+    () => '<h1>Branding</h1>'
+  )
+
+  .add(
+    'Typography',
+    () => '<h1>Hello World</h1><h2>Hello World</h2><h3>Hello World</h3><h4>Hello World</h4>'
+  )
+
+  .add(
+    'Colors',
+    () => '<h1>Colors</h1>'
+  )
+
+  .add(
+    'Shadows',
+    () => '<h1>Shadows</h1>'
+  )
+
+  .add(
+    'Icons',
+    () => '<h1>Icons</h1>'
+  )
+
+  .add(
+    'Buttons',
+    () => '<h1>Buttons</h1>'
+  )
+
+  .add(
+    'Grid',
+    () => '<h1>Grid</h1>'
+  )
+
+  .add(
+    'Forms',
+    () => '<h1>Forms</h1>'
+  )
+
+  .add(
+    'Labels',
+    () => '<h1>Labels</h1>'
+  )
+
+  .add(
+    'Motion',
+    () => '<h1>Motion</h1>'
+  )
+
+  .add(
+    'Avatars',
+    () => '<h1>Avatars</h1>'
+  )
+
+  .add(
+    'Spaces',
+    () => '<h1>Spaces</h1>'
+  )
+
+  .add('button', () => {
+    const button = document.createElement('button');
+    button.className = 'btn';
+    button.innerHTML = 'Hello Button';
+    button.addEventListener('click', action('Click'));
+    return button;
+  });
